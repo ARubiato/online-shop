890e891d884ccb3ad16a7737d415708c4df7a07a
unknown <alessanderrubiato@yahoo.com>
2018-10-26 16:31:44 +0800

    added a list of shops available

--- index.php
+++ index.php
@@ -10,6 +10,23 @@ require 'php/session.php';
     <title>Home</title>
 </head>
 <body>
+    <div class = "container-fluid">
+        <div class = "card col-lg-8 mx-auto">
+            <div class = "card-body">
+            <?php 
+                    $sql = "SELECT * FROM `user-shop`";
+                    $res = mysqli_query($conn,$sql);
+                    ?>
+                    <?php while($data = mysqli_fetch_array($res)) {?>
+                    <div class = "panel panel-default">
+                    <div class = "panel-body">
+                    <h4><?php echo $data['us_fn'];?></h4>
+                    <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     
+    </div>
 </body>
 </html>
