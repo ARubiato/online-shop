2884d0ddaf1d05d10677cecbcf9951ee5500533e
unknown <alessanderrubiato@yahoo.com>
2018-10-26 16:33:26 +0800

    commented out an input form

--- shop-create.php
+++ shop-create.php
@@ -25,26 +25,26 @@ require 'php/session.php';
             <form method = "post" action = "php/functions.php">
                   <div class = "input-group mb-3">
                     <legend>Shop name</legend> 
-                    <input type = "text" class = "form-control" name = "us_sn" placeholder = "Owner's shop">
+                    <input type = "text" class = "form-control" name = "us_sn" placeholder = "Owner's shop" required>
                 </div>
                 <div class = "input-group mb-3">
                     <legend>Shop owner's name</legend>
-                    <input type = "text" class = "form-control" name = "us_fn" placeholder = "Fullname">
+                    <input type = "text" class = "form-control" name = "us_fn" placeholder = "Fullname" required>
                 </div>
                 <div class = "input-group mb-3">
                     <legend>Shop address</legend>
-                    <input type = "text" class = "form-control" name = "us_address" placeholder = "Street">
+                    <input type = "text" class = "form-control" name = "us_address" placeholder = "Street" required>
                 </div>
                 <div class = "input-group mb-3">
                     <legend>Email</legend>
-                    <input type = "text" class = "form-control" name = "us_email" placeholder = "Email">
+                    <input type = "text" class = "form-control" name = "us_email" placeholder = "Email" required>
                 </div>
             </div>
        
             <div class = "card-body col-lg-6 mt-5">
                 <div class = "form-group">
                 <p>Choose an image file of your shop/logo</p>
-                    <input type = "file" name = "us_file_up">
+                    <!-- <input type = "file" name = "us_file_up"> -->
                 </div>
             </div>
 
